2f92f8582810e97db59a22659b19e8f39eed4c1c
anucode13 <anucode13@gmail.com>
2018-11-18 23:09:21 +1100

    Adding Logging Library again

---
+++ Solentive.Interview.Logging/Interfaces/ILoggingService.cs
@@ -0,0 +1,8 @@
+﻿namespace Solentive.Interview.Logging.Interfaces
+{
+    public interface ILoggingService
+    {
+        void LogMessage(string message);
+        void LogError(string message);
+    }
+}
---
+++ Solentive.Interview.Logging/LoggingService.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Web.Hosting;
+using Solentive.Interview.Logging.Interfaces;
+
+namespace Solentive.Interview.Logging
+{
+    public class LoggingService : ILoggingService
+    {
+        public void LogError(string message)
+        {
+            var path = Path.Combine(HostingEnvironment.MapPath("~/Logs"), "error.txt");
+            Log(message, path);
+        }
+
+        public void LogMessage(string message)
+        {
+            var path = Path.Combine(HostingEnvironment.MapPath("~/Logs"), "log.txt");
+            Log(message, path);
+        }
+
+        public static void Log(string message, string path)
+        {
+            using (var stream = new FileStream(path, FileMode.Append))
+            {
+                var writer = new StreamWriter(stream);
+                writer.WriteLine(message);
+                writer.Flush();
+            }
+        }
+
+    }
+}
