80a2b95ca87dfe37196fec2d14a160904cc450bd
anucode13 <anucode13@gmail.com>
2018-11-25 17:00:43 +1100

    Implementing Repository Pattern

---
+++ Solentive.Interview.Data/DbFactory.cs
@@ -0,0 +1,25 @@
+﻿using Solentive.Interview.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solentive.Interview.Data
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        SeminarDbContext dbContext;
+
+        public SeminarDbContext Init()
+        {
+            return dbContext ?? (dbContext = new SeminarDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
