f855d527cf5092f0d08999dc78dcd9d2fc199cc8
anucode13 <anucode13@gmail.com>
2018-11-25 16:59:56 +1100

    Implementing Repository

---
+++ Solentive.Interview.Data/Interfaces/IDbFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solentive.Interview.Data.Interfaces
+{
+    public interface IDbFactory : IDisposable
+    {
+        //DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        SeminarDbContext Init();
+    }
+}
